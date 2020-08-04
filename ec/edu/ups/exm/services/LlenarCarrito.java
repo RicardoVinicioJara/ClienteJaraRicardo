65ba8b0c1dc3c51902c0196260f9a938cecdf769
Ricardo Vinicio Jara jara <5940607-RicardoVinicioJara@users.noreply.gitlab.com>
2020-08-03 20:47:01 -0500

    Initial commit

---
+++ ec/edu/ups/exm/services/LlenarCarrito.java
@@ -0,0 +1,67 @@
+
+package ec.edu.ups.exm.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for llenarCarrito complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="llenarCarrito">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="arg0" type="{http://services.exm.ups.edu.ec/}carro" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "llenarCarrito", propOrder = {
+    "arg0"
+})
+public class LlenarCarrito {
+
+    protected List<Carro> arg0;
+
+    /**
+     * Gets the value of the arg0 property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the arg0 property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getArg0().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Carro }
+     * 
+     * 
+     */
+    public List<Carro> getArg0() {
+        if (arg0 == null) {
+            arg0 = new ArrayList<Carro>();
+        }
+        return this.arg0;
+    }
+
+}
---
+++ src/main/java/ec/edu/ups/services/Carro.java
@@ -0,0 +1,141 @@
+
+package ec.edu.ups.services;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for carro complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="carro">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="codigocompra" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="idCarro" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/>
+ *         &lt;element name="precio" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="producto" type="{http://services.exm.ups.edu.ec/}producto" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "carro", propOrder = {
+    "codigocompra",
+    "idCarro",
+    "precio",
+    "producto"
+})
+public class Carro {
+
+    protected String codigocompra;
+    protected Integer idCarro;
+    protected String precio;
+    protected Producto producto;
+
+    /**
+     * Gets the value of the codigocompra property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCodigocompra() {
+        return codigocompra;
+    }
+
+    /**
+     * Sets the value of the codigocompra property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCodigocompra(String value) {
+        this.codigocompra = value;
+    }
+
+    /**
+     * Gets the value of the idCarro property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getIdCarro() {
+        return idCarro;
+    }
+
+    /**
+     * Sets the value of the idCarro property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setIdCarro(Integer value) {
+        this.idCarro = value;
+    }
+
+    /**
+     * Gets the value of the precio property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPrecio() {
+        return precio;
+    }
+
+    /**
+     * Sets the value of the precio property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPrecio(String value) {
+        this.precio = value;
+    }
+
+    /**
+     * Gets the value of the producto property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Producto }
+     *     
+     */
+    public Producto getProducto() {
+        return producto;
+    }
+
+    /**
+     * Sets the value of the producto property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Producto }
+     *     
+     */
+    public void setProducto(Producto value) {
+        this.producto = value;
+    }
+
+}
